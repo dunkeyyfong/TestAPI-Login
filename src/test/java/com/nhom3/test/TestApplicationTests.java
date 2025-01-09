d561bcae5307493a25fe1022dbc10ff1bbe0eeab
Dunkeyy Fong <phongtrong865@gmail.com>
2025-01-09 16:50:16 +0700

    upload file

---
+++ src/test/java/com/nhom3/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom3.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
