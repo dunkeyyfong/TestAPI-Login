e606406c08b45edd951671fa36531e14e24d54a4
Dunkeyy Fong <phongtrong865@gmail.com>
2025-01-09 17:44:38 +0700

    Add HelloController and exclude DataSourceAutoConfiguration; update MongoDB URI

--- src/main/java/com/nhom3/test/TestApplication.java
+++ src/main/java/com/nhom3/test/TestApplication.java
@@ -2,12 +2,23 @@ package com.nhom3.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@RestController
+	static class HelloController {
+		@GetMapping("/hello")
+		public String sayHello() {
+			return "Hello World";
+		}
 	}
 
 }
